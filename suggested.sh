e9b05d10f93221bc83e867c4ca14218e0de1af24
Franklin Yu <franklinyu@hotmail.com>
2015-06-22 13:12:41 -0400

    add RVM, Ruby and RoR

--- suggested.sh
+++ suggested.sh
@@ -65,3 +65,12 @@ git config --global core.pager 'less -x1,5'
 
 # set default editor as Vim
 git config --global core.editor vim
+
+############################
+# RVM, Ruby, Ruby on Rails #
+############################
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+# For all in one installation append `--rails` or `--ruby` or `--ruby=1.9.3`.
+# To run in command line, use `\curl` instead of `curl` to dealias.
