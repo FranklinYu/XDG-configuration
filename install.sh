556a1f85df5bcd7d43d8c4ea77baabbacd2402c8
Franklin Yu <franklinyu@hotmail.com>
2015-06-25 13:32:12 -0700

    use verbose option name

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ZIP=`mktemp --suffix='.zip'`
-FOLDER=`mktemp -d`
+FOLDER=`mktemp --directory`
 if ! wget https://github.com/franklinyu/debian-setup/archive/master.zip -O $ZIP; then
   echo "\033[1;33mCannot download file from GitHub; check you Internet connection.\033[0m"
   exit 1
@@ -8,4 +8,4 @@ fi
 unzip -d $FOLDER $ZIP
 rm $ZIP
 sh $FOLDER/debian-setup-master/suggested.sh
-rm -r $FOLDER
+rm --recursive $FOLDER
