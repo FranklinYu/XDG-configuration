e75c7c1f3045a539e4cfd1f98c65d6cd45621536
Franklin Yu <franklinyu@hotmail.com>
2015-06-25 11:29:13 -0400

    run `chsh` in script

--- suggested.sh
+++ suggested.sh
@@ -13,7 +13,8 @@ sudo apt-get install zsh zsh-doc --assume-yes
 
 # set Z-shell as default shell
 # (from http://superuser.com/a/231736 )
-chsh -s $(which zsh)
+USR_NAME=`id --user --name`
+chsh -s $(which zsh) $USR_NAME
 
 # install Oh-My-Zsh
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
