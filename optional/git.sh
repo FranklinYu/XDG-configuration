d88cfaf8bb7a16431d39d500efd70ed8f3b41b5d
Franklin Yu <franklinyu@hotmail.com>
2015-06-28 22:34:13 -0700

    SSH key pairs

--- optional/git.sh
+++ optional/git.sh
@@ -1,3 +1,8 @@
 # add username and email (use yours, of course)
 git config --global user.name 'Franklin Yu'
 git config --global user.email franklinyu@hotmail.com
+
+# create SSH pairs (need to add the public key to the online accounts)
+ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N '' -C franklinyu@hotmail.com
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
