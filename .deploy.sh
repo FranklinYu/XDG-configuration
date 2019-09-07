913c1341522288c7f4f2bdf2540a8429913f2769
Franklin Yu <franklinyu@hotmail.com>
2019-09-07 00:00:15 -0400

    Add IRB

--- .deploy.sh
+++ .deploy.sh
@@ -1,6 +1,9 @@
+set -o errexit
+
 folder=$(cd `dirname "$0"` && pwd)
 ln -f -s "$folder/vim/vimrc" ~/.vimrc
 ln -f -s "$folder/zsh/newuser-install.zsh" ~/.zshrc
 ln -f -s "$folder/zsh/zprofile.zsh" ~/.zprofile
 
 mkdir -p ~/.cache/zsh
+mkdir -p ~/.cache/irb
