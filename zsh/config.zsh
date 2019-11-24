95a01fce1540b95452487b246b1f46442951d4a0
Franklin Yu <franklinyu@google.com>
2019-11-23 20:21:57 -0800

    Bind keys for Zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -5,4 +5,10 @@ alias la='ls --almost-all'
 
 PROMPT='%F{blue}%~%f %(?.%F{green}.%F{red})%#%f '
 
+bindkey '\e[5~' history-beginning-search-backward # PgUp
+bindkey '\e[6~' history-beginning-search-forward # PgDn
+bindkey '\e[3~' delete-char # Delete
+bindkey '\e[H' beginning-of-line # Home
+bindkey '\e[F' end-of-line # End
+
 [[ -f ~/.config/zsh/local-config.zsh ]] && source ~/.config/zsh/local-config.zsh
