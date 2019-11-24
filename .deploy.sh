8b816823c4a833fae1a4f145bac1e9534cc14aee
Franklin Yu <franklinyu@google.com>
2019-11-23 20:17:06 -0800

    Set up Readline

--- .deploy.sh
+++ .deploy.sh
@@ -1,6 +1,7 @@
 set -o errexit
 
 folder=$(cd `dirname "$0"` && pwd)
+ln -f -s "$folder/readline/inputrc" ~/.inputrc
 ln -f -s "$folder/vim/vimrc" ~/.vimrc
 ln -f -s "$folder/zsh/newuser-install.zsh" ~/.zshrc
 ln -f -s "$folder/zsh/zprofile.zsh" ~/.zprofile
