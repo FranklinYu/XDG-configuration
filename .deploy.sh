e684fd856397f021b07910a65025c00dc7c274a6
Franklin Yu <franklinyu@google.com>
2019-11-27 09:20:33 -0800

    Add tmux
    Note that terminal cannot be set as "tmux-*" because of Zsh
    autosuggestions. See
    
    https://github.com/zsh-users/zsh-autosuggestions/issues/435

--- .deploy.sh
+++ .deploy.sh
@@ -2,6 +2,7 @@ set -o errexit
 
 folder=$(cd `dirname "$0"` && pwd)
 ln -f -s "$folder/readline/inputrc" ~/.inputrc
+ln -f -s "$folder/tmux/tmux.conf" ~/.tmux.conf
 ln -f -s "$folder/vim/vimrc" ~/.vimrc
 ln -f -s "$folder/zsh/newuser-install.zsh" ~/.zshrc
 ln -f -s "$folder/zsh/zprofile.zsh" ~/.zprofile
