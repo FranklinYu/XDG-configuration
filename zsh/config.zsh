0864f465d3e5ac8f1ee9eb032617831122a7a289
Franklin Yu <franklinyu@google.com>
2019-12-03 20:47:57 -0800

    Use Vim as "less" when possible

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -3,6 +3,12 @@ alias be='bundle exec'
 alias ls='ls --color=auto' ll='ls -l --human-readable' la='ls --almost-all'
 alias docker-shell='docker run --rm --interactive --tty --volume "$PWD:/docker"'
 
+if [ -f /usr/share/vim/vim81/macros/less.sh ]
+then alias less=/usr/share/vim/vim81/macros/less.sh
+elif [ -f /usr/share/vim/vim80/macros/less.sh ]
+then alias less=/usr/share/vim/vim80/macros/less.sh
+fi
+
 PROMPT='%F{blue}%~%f %(?.%F{green}.%F{red})%#%f '
 
 bindkey '\e[5~' history-beginning-search-backward # PgUp
