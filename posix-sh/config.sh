68a11f4cc3cfd30e4a50e9763b946409ef5b613a
Franklin Yu <franklinyu@google.com>
2020-05-13 23:31:36 -0700

    Set default ping count
    It can be overridden by another `-c` flag. Endless ping is almost never
    wanted.

--- posix-sh/config.sh
+++ posix-sh/config.sh
@@ -2,6 +2,7 @@ alias g=git
 alias be='bundle exec'
 alias ls='ls --color=auto' ll='ls -l --human-readable' la='ls --almost-all'
 alias docker-shell='docker run --rm --interactive --tty --volume "$PWD:/docker"'
+alias ping='ping -c 10'
 
 if [ -f /usr/share/vim/vim81/macros/less.sh ]
 then alias less=/usr/share/vim/vim81/macros/less.sh
