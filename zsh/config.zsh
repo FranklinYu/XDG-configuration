6ff66cf70f830de78ded8071a016cea3e94f5bce
Franklin Yu <franklinyu@google.com>
2020-07-12 00:22:29 -0700

    Report CPU-angry commands in Zsh
    Note that “sleep” doesn’t consume much CPU, so it won’t be reported.
    
    http://zsh.sourceforge.net/Doc/Release/Parameters.html#Parameters-Used-By-The-Shell

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -3,6 +3,7 @@ then emulate sh -c 'source ~/.config/posix-sh/config.sh'
 fi
 
 PROMPT='%F{blue}%~%f %(?.%F{green}.%F{red})%#%f '
+REPORTTIME=10
 WORDCHARS=${WORDCHARS/\/}
 
 bindkey '\e[5~' history-beginning-search-backward # PgUp
