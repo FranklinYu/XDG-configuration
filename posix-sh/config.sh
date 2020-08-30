eb8e022e563b8d3228b471169eadfb2f202d68e5
Franklin Yu <franklinyu@google.com>
2020-08-29 23:32:41 -0700

    Alias ls commands to exa only when available
    Note that 'ls' is excluded because of chruby. See:
    
    https://github.com/postmodern/chruby/issues/413

--- posix-sh/config.sh
+++ posix-sh/config.sh
@@ -1,8 +1,13 @@
 alias g=git
 alias be='bundle exec'
-alias ls='ls --color=auto' ll='ls -l --human-readable' la='ls --almost-all'
+alias ls='ls --color=auto'
 alias docker-shell='docker run --rm --interactive --tty --volume "$PWD:/docker"'
 alias ping='ping -c 10'
+
+if command -v exa >/dev/null 2>&1
+then alias ll='exa --long' la='exa --all' tree='exa --tree'
+else alias ll='ls -l --human-readable' la='ls --almost-all'
+fi
 
 if [ -f /usr/share/vim/vim81/macros/less.sh ]
 then alias vless=/usr/share/vim/vim81/macros/less.sh
